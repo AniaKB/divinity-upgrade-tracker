54aa48862fca891af2ebdf6e9ec58eef9a86cd34
AniaKB <aniakbennett@gmail.com>
2024-01-09 16:09:14 -0500

    adding first skills to skills object in Aerotheurge

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 function generateProfile () {
   return {
+    skills: [
+      "name": "Aerothurge",
+      "skills": {
+        "1": [
+          {
+            "name": "Blinding Radiance",
+            "description": "Blinding Radiance sets \"Radiant\" status which inflicts Blinded status upon enemies without Magic Armour as well as dealing air damage upon casting",
+            "action points": 2,
+            "source points": 0,
+            "required": "Aerotheurge 1"
+          },
+          {
+            "name": "Electric Discharge",
+            "description": "Deal air damage and set Shocked for two turns on enemies without Magic Armour",
+            "action points": 2,
+            "source points": 0,
+            "required": "Aerotheurge 1"
+          },
+          {
+            "name": "Favorable Wind",
+            "description": "Sets Favorable Wind Aura on the caster and allies within close proximity for four turns, increasing their movement speed by 1.5m for 4 turns.",
+            "action points": 1,
+            "source points": 0,
+            "required": "Aerotheurge 1"
+          },
+          {
+            "name": "Shocking Touch",
+            "description": "Touch your enemy to deal them air damage and set Stunned them if they dont have Magic Armour.",
+            "action points": 1,
+            "source points": 0,
+            "required": "Aerotheurge 1"
+          },
+        ],
+        "2": [
+
+        ]
+      }
+    ]
 
   }
 }
