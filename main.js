5aa7132095d191fe2fdc2f1d0d9fd7a175eaad91
Ania <aniakbennett@gmail.com>
2024-01-16 22:22:33 -0500

    adding more data to objects

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let items = [
+let items = [ 
   "Ancient human skull",
   "A jar of mind maggots",
   "Air essence",
@@ -134,10 +134,10 @@ let classes = [
 function generateProfile () {
   return {
     skills: [
-    {
+{
       "name": "Aerothurge",
       "skills": {
-        "1": [
+        "level 1": [
           {
             "name": "Blinding Radiance",
             "img": "photos/aerothurge/Blinding_Radiance.webp",
@@ -148,6 +148,7 @@ function generateProfile () {
           },
           {
             "name": "Electric Discharge",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
             "description": "Deal air damage and set Shocked for two turns on enemies without Magic Armour",
             "action points": 2,
             "source points": 0,
@@ -155,6 +156,7 @@ function generateProfile () {
           },
           {
             "name": "Favorable Wind",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
             "description": "Sets Favorable Wind Aura on the caster and allies within close proximity for four turns, increasing their movement speed by 1.5m for 4 turns.",
             "action points": 1,
             "source points": 0,
@@ -162,17 +164,67 @@ function generateProfile () {
           },
           {
             "name": "Shocking Touch",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
             "description": "Touch your enemy to deal them air damage and set Stunned them if they dont have Magic Armour.",
             "action points": 1,
             "source points": 0,
             "required": "Aerotheurge 1"
           },
         ],
-        "2": [
-
-        ]
+        "level 4": [
+          {
+            "name": "Dazing Bolt",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
+            "description": "Shoot an electrical bolt from the sky dealing air damage to characters and items in the area, and setting Shocked status.",
+            "action points": 3,
+            "source points": 0,
+            "required": "Aerotheurge 2"
+          },
+          {
+            "name": "Teleportation",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
+            "description": "Move objects (inanimate or otherwise) from one location to another. Deals physical damage upon impact.",
+            "action points": 2,
+            "source points": 0,
+            "required": "Aerotheurge 2"
+          },
+          {
+            "name": "Uncanny Evasion",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
+            "description": "Increases dodging by 90% and movement by 20%.",
+            "action points": 1,
+            "source points": 0,
+            "required": "Aerotheurge 2"
+          },
+        ],
+        "level 9": [
+          {
+            "name": "Chain Lightning",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
+            "description": "Target enemy receives air damage. The spell then forks up to eight times up to 8m of the original impact zone dealing air damage to each enemy struck.",
+            "action points": 3,
+            "source points": 1,
+            "required": "Aerotheurge 3"
+          },
+          {
+            "name": "Nether Swap",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
+            "description": "Nether swap allows the caster or an ally of their choosing to swap places with an enemy.",
+            "action points": 2,
+            "source points": 0,
+            "required": "Aerotheurge 2"
+          },
+          {
+            "name": "Pressire Spike",
+            "img": "photos/aerothurge/Blinding_Radiance.webp",
+            "description": "Pressure Spike condenses all cloud surfaces in the area. Deals air damage to enemies and douses fires.",
+            "action points": 1,
+            "source points": 0,
+            "required": "Aerotheurge 2"
+          },
+        ],
       },
-      sdfgh: {
+      {
         "name": "Polymorph",
         "skills": [
           {
