d2e34b2d6c0bce6365561e67df0a4632c547695f
Ania <aniakbennett@gmail.com>
2024-01-23 16:56:09 -0500

    updating item photos

---
+++ crafting.js
@@ -0,0 +1,238 @@
+Runes:
+Bone + Mortar & Pestle = Bone dust
+Stardust Herb + Mortar & Pestle = Star dust
+Bone dust + Star dust = Pixie Dust
+Wood + Oil + Pixie Dust = Small Flame Rune
+Water + Gold + Pixie Dust = Small Thunder Rune
+Water + Metal + Pixie Dust = Small Frost Rune
+Rock + Beer + Pixie Dust = Small Rock Rune
+Bone + Poison + Pixie Dust = Small Venom Rune
+Livewood Log + Wine + Pixie Dust = Small Masterwork Rune
+
+Any small rune combined with itself = Medium rune of that type
+2 Medium runes of the same type = Large rune of that type
+3 Medium runes of the same type = Giant rune of that type
+2 Large runes = Giant rune of that type
+
+Runes are currently useless I think
+
+Making bigger Potions:
+All potions seem to work using the following formula:
+2 Minor = Medium
+Minor + Medium = Normal
+2 Medium = Normal
+Medium + Normal = Huge / Large
+2 Normal = Huge / Large
+1 Augmentor + Minor/Medium = Normal
+1 Augmentor + Normal = Large
+1 Augmentor + Large = Huge
+
+So for eg 2 Normal healing potions will make a huge however, so will 1 medium and 1 normal. If you're crafting them then create a stack of both and use those for min maxing.
+With that in mind I'll not include potions that combine with themselves since it creates a tl;dr post
+Stat boost potions do not follow the same rules.
+
+
+Poison Potion + Red Dye = Poison Potion (That looks like a healing potion)
+
+Herb Combinations for potions:
+Empty bottle + Fly Agaric = poison
+Empty bottle + Penny Bun = Minor healing
+Empty bottle + Yarrow = Healing Elixir
+Empty bottle + Whisperwood = Magic armour
+Empty bottle + Amadouvier = Physical armour
+Empty bottle + Guepinia = Fire resistance
+Empty bottle + Bluegill = Water resistance
+Empty bottle + Jellyroom = Air resistance
+Empty bottle + Earth tongue = Earth Resistance
+Empty bottle + Trumpet Of Death = Minor Resist all
+Empty bottle + Amethyst Deceiver = Minor Strength
+Empty bottle + Boletus = Minor Finess
+Empty bottle + Calocera = Minor Int
+Empty bottle + Farhangite = Minor Constitution
+Empty bottle + Puffball = Minor Wits
+Empty bottle + Chanterelle = Invisibility
+Empty bottle + Earth Essence = Stoneskin
+Empty bottle + Water Essence = NimbleTumble
+Empty bottle + Fire Essence = Strong Will
+Empty bottle + Air Essence = Electric Skin
+Empty bottle + BlackRose = Black Rose Elixir
+
+
+Grenades:
+Intestines + Water = Water Balloon
+Empty Canister + Jellyroom = Razzle Dazzle
+Empty Canister + Air Resistence = Razzle Dazzle
+Empty Canister + Intestines = Chemical Warfare
+Empty Canister + Tormented Soul = Terror
+Empty Canister + Earth Essence = Tremor
+Empty Canister + Air Essence = Thunderbolt
+Empty Canister + Water Essence = Frost
+Empty Canister + Fire Essence = Cluster
+Empty Canister + Poison = Gas
+Empty Grenade + Nails = Nailbomb
+Empty Grenade + Broken Bottle = Nailbomb
+Empty Perfume Bottle + Honey = Love Grenade
+Empty Perfume Bottle + Pixie Dust = Love Grenade
+Empty Holy Grenade + Starfish = Holy Bomb
+Empty Holy Grenade + Pixie Dust = Holy Bomb
+Empty Flask + Poison = Poison Flask
+Empty Flask + Oil = Oil Flask
+Empty Bottle + Oil = Bottle of Oil
+Bottle of Oil + Fuse = Firestorm Grenade
+Source Orb + Water Balloon = Blessed Water Baloon
+Source Orb + Ice Grenade = Blessed Ice Grenade
+Source Orb + Firestorm Grenade = Cursed Firestorm Grenade
+Source Orb + Cluster Bomb = Cursed Cluster Bomb
+Source Orb + Oil Flask = Blessed Oil Flask
+Source Orb + Poison Flask = Cursed poison flask
+
+Arrow Heads:
+Cutting Tool + Tooth = Stunning
+Cutting Tool + Antler = Knockdown
+Arrow Head + Oil = Slowdown
+Arrow Head + Water = Water
+Arrow Head + Poison = Poison
+Arrow Head + Honey = Charming
+Arrow Head + Essence of Fire = Fire
+Arrow Head + Essence of Water = Freezing
+Arrow Head + Essence of Earth = Poison Cloud
+Arrow Head + Essence of Air = Static cloud
+Fire Arrowhead + Oil = Explosive
+Fire Arrowhead + Water = Steamcloud
+Source Orb + Water Arrow/Arrowhead = Blessed Water
+Source Orb + Fire Arrow/Arrowhead = Cursed Fire
+
+Combine these with arrow shaft for arrows:
+Cutting Tool + Wooden Branch = Arrow Shaft
+
+Scrolls:
+Fire:
+Paper Sheet + Essence of Fire + Tooth = Flaming Daggers
+Paper Sheet + Essence of Fire + Claw = Haste ???
+Paper Sheet + Essence of Fire + Tusk = Ignition
+Paper Sheet + Essence of Fire + Sharp Claw = Fireball
+Paper Sheet + Essence of Fire + Antler = Wildfire
+Paper Sheet + Essence of Fire + Sharp Tooth = Infectious Flame ???
+Paper Sheet + Essence of Fire + Large Tusk = Infectious Flame ???
+Water:
+Paper Sheet + Essence of Water + Herring = Rain
+Paper Sheet + Essence of Water + Plaice = Winter Blast
+Paper Sheet + Essence of Water + Bluegill = Armour of Frost
+Paper Sheet + Essence of Water + Essence of Life = Resurrection
+Paper Sheet + Essence of Water + Fish C = Hailstrike
+Paper Sheet + Essence of Water + Penny Bun = Regeneration
+Paper Sheet + Fish C + Source Orb = Steam Lance
+Earth:
+Paper Sheet + Essence of Earth + Whisperwood = Fortify
+Paper Sheet + Essence of Earth + Piece of rock = Fossil Strike
+Paper Sheet + Essence of Earth + Metal Shard = Impalement
+Paper Sheet + Essence of Earth + Intestines = Contamination
+Paper Sheet + Essence of Earth + Fly Ageric = Poison Dart
+Paper Sheet + Essence of Earth + Earth Tongue = Acid Spores
+Paper Sheet + Source Orb + Rock = Fossil Strike
+Air:
+Paper Sheet + Essence of Air + Feather = Teleportation
+Paper Sheet + Essence of Air + Nails = Shocking Touch
+Paper Sheet + Essence of Air + Flour = Favourable WInd
+Paper Sheet + Essence of Air + Gold = Blinding Radiance
+Paper Sheet + Essence of Air + Rabbit Paw = Electric Discharge
+Paper Sheet + Essence of Air + Hair = Tornado
+Paper Sheet + Essence of Air + Chanterelle = Netherswap
+Paper Sheet + Source Orb + Wool/Wool Yarn = Chain Lightning
+Shadow:
+Paper Sheet + Essence of Shadow + Intestines = Infect
+Paper Sheet + Essence of Shadow + Penny Bun = Blood Sucker
+Paper Sheet + Essence of Shadow + Skull = Shackles of pain
+Paper Sheet + Tormented Soul + Pepper = Dimensional Bolt
+Paper Sheet + Tormented Soul + Wheat = Soul Mate
+Paper Sheet + Tormented Soul + Garlic = Rallying Cry
+Paper Sheet + Tormented Soul + Honey = Charm?
+Paper Sheet + Tormented Soul + Wood Figureen = Elemental Totem
+Paper Sheet + Source Orb + Carrot = Ethereal Storm?
+Paper Sheet + Source Orb + Grapes = Planar Gateway?
+Life:
+Paper Sheet + Essence of Life + Chicken Foot = Chicken Touch
+Paper Sheet + Essence of Life + Sinew = Spread your wings
+Paper Sheet + Essence of Life + rat tail = tentacle lash
+Paper Sheet + Essence of Life + Anthropod Leg = Spider Legs
+Paper Sheet + Essence of Life + Oil = Summon Oil
+Paper Sheet + Essence of Life + Steel Skin
+Paper Sheet + Essence of Life + Animal Scales = Skin Graft
+Paper Sheet + Essence of Life + Antler = Bullhorns
+Paper Sheet + Source Orb + Animal Scales = Medusa Head ???
+
+Skill books:
+Any empty skill book + the scroll of that type
+
+Weapons:
+Wooden Stick + Rock = Crafted Axe (1H)
+Wooden Branch + Rock = Crafted Axe (2H)
+Wooden Stick + Bowstring = Bow
+Wooden Stick + Wooden Branch = Crossbow
+Metal Shard + Scraps of cloth/leather = Shiv
+Wooden Branch + Scraps of cloth/leather = Makeshift Club
+Wooden Branch + Shell = Staff (Random element: Fire/air/earth/water)
+Tongs + Scraps of cloth/leather = Crafted Mace (2H)
+Wooden Stick + Skull = Wand (Random Element: Fire/air/earth/water)
+
+Buffs:
+Poison Potion + Weapon = Poison weapon
+Any Shoes + nails = Winter boots (Immunity to slipping)
+
+Tools:
+Needles + Thread = Needle & thread
+2 Hair = Thread
+Cutting tool + Animal Hide = Leather Scraps
+Cutting tool + Log = Wood chips / branch?
+Cutting tool + Branch = Wood chips / stick?
+Repair Hammer + Nails = Lockpick
+Soap + Any key = Lockpick
+2 Needles = Lockpick
+Rope + Scraps of leather = Backpack
+Cutting tool + Pillow = feather
+2 Wool = yarn
+2 Yarn = Rope
+Wood chips + water = Wood mush?
+Wood Mush + Cooking station = Paper?
+Needle + Wood figureen = Inert Voodo doll
+Voodoo doll + Pixie dust = Poorly made voodo doll
+
+Armour:
+Needle & thread + Cloth Scraps = Cloth Shirt
+Needle & thread + Cloth Scraps + Pixie Dust = Mage Armour
+Needle & thread + Leather Scraps = Leather Armour
+Needle & thread + Metal Scraps = Chain Armour
+Anvil + Scale scraps = Scale Armour
+Anvil + Plate scraps = Plate armour
+
+Food:
+Cooking pot + campfire = cooking station
+Mortar & Pestle + Wheat = Flour
+Flour + Water = Dough
+Empty Bottle/Mug/Cup + Water = Bottle/Mug/Cup of water
+Empty Bottle/Mug/Cup + Beer = Bottle/Mug/Cup of beer
+Empty Bottle/Mug/Cup + Grapes/wine = Bottle/Mug/Cup of wine
+Repair Hammer + Tomato = Tomato Sauce
+Tomato Sauce + Dough = Pizza Dough
+Cheese + Dough = Cheese dough
+Apple + Dough = Apple Dough
+Empty Jar + Behive = Jar of Honey
+Jar of Honey + Milk = Milk Honey
+Wheat + Cup of milk = Porridge
+Potato + Cup of milk = Potato Porridge
+Cutting tool + Potato = Cold Fries
+Cold fries + Cooking station = Rivellon Fries
+Potato + Cooking station = Boiled Potato
+Potato + Repair Hammer = Cold Mash Potato + Cooking station = Mash Potato
+Cold Mash Potato + Cooking station = Mash Potato
+Dinner + Tomato sauce = Elven Stew
+Dinner + Mug of beer = Dwavern Stew
+Pumpkin + Cooking station = Pumpkin soup
+Cooking station + Dough = Bread
+Cooking station + Pizza Dough = Pizza
+Cooking station + Cheese dough = Cheese bread
+Cooking station + apple dough = Apple pie
+Fish + Dough = Cyseal dough
+Cooking station + Cyseal dough = Cyseal Pie
+Empty Cup/Mug + Apple = Apple juice
+Empty Cup/Mug + Tea Leaves = Tea
