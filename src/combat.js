e0af0dc4a5218a46decf46d6b040ff7a772a8d33
AniaKB <aniakbennett@gmail.com>
2024-01-30 16:04:48 -0500

    fixing styling

---
+++ src/combat.js
@@ -0,0 +1,28 @@
+let combat = [
+  Weapons:
+  {
+    Dual Wielding: 0,
+    Ranged: 0,
+    'Single-Handed': 0,
+    'Two-Handed': 0,
+  },
+  Defense:
+  {
+    Leadership: 0,
+    Perseverance: 0,
+    Retribution: 0,
+  },
+  Skills:
+  {
+    Aerotheurge: 0,
+    Geomancer: 0,
+    Huntsman: 0,
+    Hydrosophist: 0,
+    Necromancer: 0,
+    Polymorph: 0,
+    Pyrokinetic: 0,
+    Scoundrel: 0,
+    Summoning: 0,
+    Warfare: 0,
+  }
+];
---
+++ src/origin.js
@@ -0,0 +1,79 @@
+Let origin = [
+  {
+    origin: 'Red Prince',
+    talents: ['Dragon\'s Blaze', 'Demonic Stare'],
+    civil: ['Sophisticated', 'Spellsong' (Persuasion +1)]
+  }
+  {
+    origin: 'Sebille',
+    talents: ['Flesh Sacrifice', 'Break the Shackles'],
+    civil: ['Corpse Eater', 'Ancestral Knowledge' (Loremaster +1)],
+  },
+  {
+    origin: 'Ifan',
+    talents: ['Flesh Sacrifice', 'Break the Shackles'],
+    civil: ['Ingenious', 'thrifty' (Bartering +1)]
+  }
+  {
+    origin: 'Beast',
+    talents: ['Petrifying Touch', 'Blinding Squall'],
+    civil: ['Sturdy', 'Dwarven Guile' (Sneaking +1)]
+  }
+  {
+    origin: 'Fane',
+    talents: ['Play Dead', 'Time Warp'],
+    civil: ['Undead', 'Ingenious']
+  },
+  {
+    origin: 'Dwarf',
+    talents: ['Petrifying Touch', 'Dome of Protection'],
+    civil: ['Sturdy', 'Dwarven Guile' (Sneaking +1)]
+  }
+  {
+    origin: 'Elf',
+    talents: ['Flesh Sacrifice', 'Dome of Protection'],
+    civil: ['Corpse Eater', 'Ancestral Knowledge' (Loremaster +1)],
+  },
+  {
+    origin: 'Human',
+    talents: ['Encourage', 'Dome of Protection'],
+    civil: ['Ingenious', 'thrifty' (Bartering +1)]
+  },
+  {
+    origin: 'Lizard',
+    talents: ['Dragon\'s Blaze', 'Dome of Protection'],
+    civil: ['Sophisticated', 'Spellsong' (Persuasion +1)]
+  }
+  {
+    origin: 'Undead Dwarf',
+    talents: ['Play Dead', 'Dome of Protection'],
+    civil: ['Undead', 'Sturdy']
+  }
+  {
+    origin: 'Undead Elf',
+    talents: ['Play Dead', 'Dome of Protection'],
+    civil: ['Undead', 'Corpse Eater']
+  },
+  {
+    origin: 'Undead Human',
+    talents: ['Play Dead', 'Dome of Protection'],
+    civil: ['Undead', 'Ingenious']
+  },
+  {
+    origin: 'Undead Lizard',
+    talents: ['Play Dead', 'Dome of Protection'],
+    civil: ['Undead', 'Sophisticated']
+  }
+]
+
+let civil = [
+  {
+    Bartering: 0,
+    Lucky Charm: 0,
+    Persuasion: 0,
+    Loremaster: 0,
+    Telekinesis: 0,
+    Sneaking: 0,
+    Thievery: 0,
+  }
+]
