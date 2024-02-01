4cf4d1129c8ba5702b98055241f7913f51793bdf
AniaKB <aniakbennett@gmail.com>
2024-02-01 13:28:01 -0500

    updating js for origin tracker

--- src/origin.js
+++ src/origin.js
@@ -1,23 +1,27 @@
-Let origin = [
+let origin = [
   {
     origin: 'Red Prince',
     talents: ['Dragon\'s Blaze', 'Demonic Stare'],
-    civil: ['Sophisticated', 'Spellsong' (Persuasion +1)]
+    civil: ['Sophisticated', 'Spellsong']
+    {attribute: 'Persuasion', points: 1},
   }
   {
     origin: 'Sebille',
     talents: ['Flesh Sacrifice', 'Break the Shackles'],
-    civil: ['Corpse Eater', 'Ancestral Knowledge' (Loremaster +1)],
+    civil: ['Corpse Eater', 'Ancestral Knowledge'],
+    {attribute: 'Loremaster', points: 1},
   },
   {
     origin: 'Ifan',
     talents: ['Flesh Sacrifice', 'Break the Shackles'],
-    civil: ['Ingenious', 'thrifty' (Bartering +1)]
+    civil: ['Ingenious', 'thrifty']
+    {attribute: 'Bartering', points: 1},
   }
   {
     origin: 'Beast',
     talents: ['Petrifying Touch', 'Blinding Squall'],
-    civil: ['Sturdy', 'Dwarven Guile' (Sneaking +1)]
+    civil: ['Sturdy', 'Dwarven Guile']
+    {attribute: 'Sneaking', points: 1},
   }
   {
     origin: 'Fane',
@@ -27,22 +31,26 @@ Let origin = [
   {
     origin: 'Dwarf',
     talents: ['Petrifying Touch', 'Dome of Protection'],
-    civil: ['Sturdy', 'Dwarven Guile' (Sneaking +1)]
+    civil: ['Sturdy', 'Dwarven Guile']
+    {attribute: 'Sneaking', points: 1},
   }
   {
     origin: 'Elf',
     talents: ['Flesh Sacrifice', 'Dome of Protection'],
-    civil: ['Corpse Eater', 'Ancestral Knowledge' (Loremaster +1)],
+    civil: ['Corpse Eater', 'Ancestral Knowledge'],
+    {attribute: 'Loremaster', points: 1},
   },
   {
     origin: 'Human',
     talents: ['Encourage', 'Dome of Protection'],
-    civil: ['Ingenious', 'thrifty' (Bartering +1)]
+    civil: ['Ingenious', 'thrifty']
+    {attribute: 'Bartering', points: 1},
   },
   {
     origin: 'Lizard',
     talents: ['Dragon\'s Blaze', 'Dome of Protection'],
-    civil: ['Sophisticated', 'Spellsong' (Persuasion +1)]
+    civil: ['Sophisticated', 'Spellsong']
+    {attribute: 'Persuasion', points: 1},
   }
   {
     origin: 'Undead Dwarf',
