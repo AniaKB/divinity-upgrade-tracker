dbeee8154f1c5986d97ff3557be6820734b24f18
AniaKB <aniakbennett@gmail.com>
2024-02-19 17:31:36 -0500

    fixing attribute and combat points buttons

--- src/level.js
+++ src/level.js
@@ -254,16 +254,22 @@ updateTotalAttPoints();
 
 function updateTotalAttPoints () {
   const selectedLevel = parseInt(levelDropdown.value);
+  let total = 0;
 
-  const total = levels.slice(0, selectedLevel)
-                      .reduce((acc, curr) => acc + curr.attribute, 0);
+  for (let i = 0; i < selectedLevel; i++) {
+    total += levels[i].attribute;
+  }
+
+  attributesArray.forEach((att) => {
+    total -= att.points;
+  });
 
   totalAttPoints = total;
   totalAttPointsDisplay.textContent = `Attributes: ${totalAttPoints}`;
 }
 
 function addAttPoints (index) {
-  if (totalAttPoints > 0 && attributesArray[index].points < totalAttPoints) {
+  if (totalAttPoints > 0) {
     attributesArray[index].points++;
     totalAttPoints--;
     renderAttributes();
@@ -314,3 +320,98 @@ levelDropdown.addEventListener('change', () => {
 });
 
 renderAttributes();
+
+
+let combatArray = [
+  { combat: 'Dual Wielding', points: 0 },
+  { combat: 'Ranged', points: 0 },
+  { combat: 'Single-Handed', points: 0 },
+  { combat: 'Two-Handed', points: 0 },
+  { combat: 'Leadership', points: 0 },
+  { combat: 'Perseverance', points: 0 },
+  { combat: 'Retribution', points: 0 },
+  { combat: 'Aerotheurge', points: 0 },
+  { combat: 'Geomancer', points: 0 },
+  { combat: 'Huntsman', points: 0 },
+  { combat: 'Hydrosophist', points: 0 },
+  { combat: 'Necromancer', points: 0 },
+  { combat: 'Polymorph', points: 0 },
+  { combat: 'Pyrokinetic', points: 0 },
+  { combat: 'Scoundrel', points: 0 },
+  { combat: 'Summoning', points: 0 },
+  { combat: 'Warfare', points: 0 }
+];
+
+const myCombat = document.getElementById('combat');
+const totalCombatPointsDisplay = document.getElementById('totalCombatPointsDisplay');
+let totalCombatPoints = 0;
+
+levelDropdown.value = 1;
+updateTotalCombatPoints();
+
+function updateTotalCombatPoints () {
+  let selectedLevel = parseInt(levelDropdown.value);
+  let total = 0;
+
+  for (let i = 0; i < selectedLevel; i++) {
+    total += levels[i].combat;
+  }
+  combatArray.forEach((com) => {
+    total -= com.points;
+  });
+  totalCombatPoints = total;
+  totalCombatPointsDisplay.textContent = `Combat Abilities: ${totalCombatPoints}`;
+}
+
+function addPoints (index) {
+  if (totalCombatPoints > 0) {
+    combatArray[index].points++;
+    totalCombatPoints--;
+    renderCombat();
+    updateTotalCombatPoints();
+  }
+}
+
+function removePoints (index) {
+  if (combatArray[index].points > 0) {
+    combatArray[index].points--;
+    totalCombatPoints++;
+    renderCombat();
+    updateTotalCombatPoints();
+  }
+}
+
+function renderCombat () {
+  myCombat.innerHTML = '';
+
+  combatArray.forEach((com, index) => {
+    const listCombat = document.createElement('li');
+
+    const combatText = document.createElement('span');
+    combatText.textContent = com.combat;
+    listCombat.appendChild(combatText);
+
+    const decreaseCombatButton = document.createElement('button');
+    decreaseCombatButton.textContent = '-';
+    decreaseCombatButton.addEventListener('click', () => removePoints(index));
+    listCombat.appendChild(decreaseCombatButton);
+
+    const pointsText = document.createElement('span');
+    pointsText.textContent = com.points;
+    listCombat.appendChild(pointsText);
+
+    const increaseCombatButton = document.createElement('button');
+    increaseCombatButton.textContent = '+';
+    increaseCombatButton.addEventListener('click', () => addPoints(index));
+    listCombat.appendChild(increaseCombatButton);
+
+    myCombat.appendChild(listCombat);
+  });
+}
+
+levelDropdown.addEventListener('change', () => {
+  updateTotalCombatPoints();
+  renderCombat();
+});
+
+renderCombat();
