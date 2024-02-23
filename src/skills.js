c69815cef5f49e5e618178da5e9162a249ab9e75
AniaKB <aniakbennett@gmail.com>
2024-02-23 11:46:58 -0500

    adding tooltip through JS

--- src/skills.js
+++ src/skills.js
@@ -2273,6 +2273,7 @@ function createSkillButtons () {
         subSkillButton.style.backgroundSize = 'cover';
         subSkillButton.style.backgroundPosition = 'center';
         subSkillButton.id = skill.name;
+        subSkillButton.dataset.tooltip = sublevel.tooltip;
 
         subSkillButton.addEventListener('click', () => {
           subSkillButton.classList.toggle('active');
